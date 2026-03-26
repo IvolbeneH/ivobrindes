c9c0a25533ec8da92a26f430431274e11724c67b
IvolbeneH <ivolbene1@gmail.com>
2026-03-26 20:57:29 -0300

    atualizando metadado

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,12 @@ const inter = Inter({
 export const metadata = {
   title: "Ivo Brindes - Brindes personalizados",
   description: "Brindes personalizados para empresas, eventos e campanhas.",
+  openGraph: {
+    title: "Ivo Brindes",
+    description:
+      "A Ivo Brindes é especializada em brindes personalizados para empresas e campanhas eleitorais em Teresina. Trabalhamos com copos, camisetas, adesivos, canecas e muito mais, sempre com alta qualidade, ótimo custo-benefício e entrega rápida. Solicite seu orçamento e destaque sua marca com brindes exclusivos.",
+    images: ["/logobrindes.png"],
+  },
 };
 
 export default function RootLayout({
